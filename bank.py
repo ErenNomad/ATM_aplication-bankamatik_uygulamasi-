b8870fafb1072a0fb36b131823d74d31b7b0c4d8
Eren Yörük <77466728+ErenNomad@users.noreply.github.com>
2021-12-04 16:50:36 +0300

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,6 +1,6 @@
 #ATM application
 user1_account = {
-    'name' : 'userName userSurname',
+    'name' : 'user1Name user1Surname',
     'accountNum' : '12345',
     'balance' : 5000,
     'adAccount' : 3000
